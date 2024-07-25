36ddbe5e67f75ef49fbad9c06370256778522241
analuisa <analuisaaugustob@gmail.com>
2024-07-25 15:19:30 -0300

    :sparkle:feat: Implementando testes do Strategy.

--- src/one/digitalinnovation/gof/Teste.java
+++ src/one/digitalinnovation/gof/Teste.java
@@ -1,6 +1,18 @@
 package one.digitalinnovation.gof;
 
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.Comportamento;
+import one.digitalinnovation.gof.strategy.ComportamentoAgressivo;
+import one.digitalinnovation.gof.strategy.ComportamentoDefensivo;
+import one.digitalinnovation.gof.strategy.ComportamentoNormal;
+import one.digitalinnovation.gof.strategy.Robo;
+
 public class Teste {
+
+    // Singleton:
+
     public static void main(String[] args) {
         SingletonLazy lazy = SingletonLazy.getInstancia();
         System.out.println(lazy);
@@ -16,5 +28,24 @@ public class Teste {
         System.out.println(lazyHolder);
         lazyHolder = SingletonLazyHolder.getInstancia();
         System.out.println(lazyHolder);
+
+        // Strategy
+
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setStrategy(normal);
+        robo.mover();
+        robo.mover();
+
+        robo.setStrategy(defensivo);
+        robo.mover();
+
+        robo.setStrategy(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
     }
 }
